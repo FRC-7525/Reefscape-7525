1b255f99f93eb36b9c1e9e1d01be83008eb4cc9a
Otto <grentomato@gmail.com>
2025-01-05 20:16:04 -0600

    Adds Drive

---
+++ src/main/java/frc/robot/subsystems/Drive/DriveIOReal.java
@@ -0,0 +1,82 @@
+package frc.robot.subsystems.Drive;
+
+import static frc.robot.subsystems.Drive.TunerConstants.*;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+
+/**
+ * This class implements the DriveIO interface and provides the real implementation for controlling the drivetrain.
+ */
+public class DriveIOReal implements DriveIO {
+
+	public DriveIOInputs inputs = new DriveIOInputs();
+	private final TunerSwerveDrivetrain drivetrain = new TunerSwerveDrivetrain(
+		DrivetrainConstants,
+		FrontLeft,
+		FrontRight,
+		BackLeft,
+		BackRight
+	);
+
+	/**
+	 * Updates the inputs for the drivetrain based on the current state of the SwerveDrivetrain.
+	 * @param inputs The DriveIOInputs object to update.
+	 */
+	@Override
+	public void updateInputs(DriveIOInputs inputs) {
+		inputs.speeds = drivetrain.getState().Speeds;
+		inputs.setPoints = drivetrain.getState().ModuleTargets;
+		inputs.odometryFrequency = drivetrain.getOdometryFrequency();
+		inputs.timestamp = drivetrain.getState().Timestamp;
+		inputs.failedDataAquisitions = drivetrain.getState().FailedDaqs;
+		inputs.robotAngleDeg = drivetrain.getState().Pose.getRotation().getDegrees();
+		inputs.fullRobotRotation = drivetrain.getRotation3d();
+		inputs.gyroAngleDeg = drivetrain.getPigeon2().getYaw().getValueAsDouble();
+	}
+
+	/**
+	 * Gets the SwerveDrivetrain object.
+	 * @return The SwerveDrivetrain object.
+	 */
+	@Override
+	public TunerSwerveDrivetrain getDrive() {
+		return drivetrain;
+	}
+
+	/**
+	 * Resets the gyro angle to zero.
+	 */
+	@Override
+	public void zeroGyro() {
+		drivetrain.resetRotation(new Rotation2d());
+	}
+
+	/**
+	 * Sets the control mode and target for the SwerveDrivetrain.
+	 * @param request The SwerveRequest object containing the control mode and target.
+	 */
+	@Override
+	public void setControl(SwerveRequest request) {
+		drivetrain.setControl(request);
+	}
+
+	/**
+	 * Adds a vision measurement to the SwerveDrivetrain for localization.
+	 * @param pose The Pose2d object representing the measured pose.
+	 * @param timestamp The timestamp of the measurement.
+	 * @param standardDeviaton The standard deviation of the measurement.
+	 */
+	@Override
+	public void addVisionMeasurement(
+		Pose2d pose,
+		double timestamp,
+		Matrix<N3, N1> standardDeviaton
+	) {
+		drivetrain.addVisionMeasurement(pose, timestamp, standardDeviaton);
+	}
+}
---
+++ src/main/java/frc/robot/subsystems/Drive/DriveStates.java
@@ -0,0 +1,81 @@
+package frc.robot.subsystems.Drive;
+
+import static edu.wpi.first.units.Units.*;
+import static frc.robot.GlobalConstants.Controllers.*;
+import static frc.robot.GlobalConstants.Drive.*;
+import static frc.robot.subsystems.Drive.TunerConstants.*;
+
+import org.team7525.subsystem.SubsystemStates;
+
+/**
+ * An enumeration representing different drive states for a robot's drive subsystem.
+ */
+public enum DriveStates implements SubsystemStates {
+	/**
+	 * The robot's drive is in field-relative mode.
+	 */
+	FIELD_RELATIVE("Field Relative", () -> {
+		Drive.getInstance()
+			.driveFieldRelative(
+				DRIVER_CONTROLLER.getLeftY() * kSpeedAt12Volts.in(MetersPerSecond),
+				DRIVER_CONTROLLER.getLeftX() * kSpeedAt12Volts.in(MetersPerSecond),
+				DRIVER_CONTROLLER.getRightX() * ANGULAR_VELOCITY_LIMIT.in(RadiansPerSecond)
+			);
+	}),
+
+	/**
+	 * The robot's drive is in robot-relative mode.
+	 */
+	ROBOT_RELATIVE("Robot Relative", () -> {
+		Drive.getInstance()
+			.driveRobotRelative(
+				DRIVER_CONTROLLER.getLeftY() * kSpeedAt12Volts.in(MetersPerSecond),
+				DRIVER_CONTROLLER.getLeftX() * kSpeedAt12Volts.in(MetersPerSecond),
+				DRIVER_CONTROLLER.getRightX() * ANGULAR_VELOCITY_LIMIT.in(RadiansPerSecond)
+			);
+	}),
+
+	/* The robot's drive is in locking wheels mode starting from field relative.
+	 */
+	LOCKING_WHEELS_FIELD("Locking Wheels", () -> {
+		Drive.getInstance().lockWheels();
+	}),
+
+	/**
+	 * The robot's drive is in locking wheels mode starting from robot relative.
+	 */
+	LOCKING_WHEELS_ROBOT("Locking Wheels Robot", () -> {
+		Drive.getInstance().lockWheels();
+	});
+
+	private String stateString;
+	private Runnable driveControl;
+
+	/**
+	 * Constructs a DriveStates enum value with the specified state string and field-relative flag.
+	 *
+	 * @param stateString    the string representation of the drive state
+	 * @param Runnable  runnable that gets called to drive the robot
+	 */
+	DriveStates(String stateString, Runnable driveControl) {
+		this.stateString = stateString;
+		this.driveControl = driveControl;
+	}
+
+	/**
+	 * Returns the string representation of the drive state.
+	 *
+	 * @return the string representation of the drive state
+	 */
+	@Override
+	public String getStateString() {
+		return stateString;
+	}
+
+	/**
+	 * Drives the robot in the current state.
+	 */
+	public void driveRobot() {
+		driveControl.run();
+	}
+}
