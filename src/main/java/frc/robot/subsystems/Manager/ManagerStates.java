b4fe3d37a31bdbbd13f105193348b5b45e168f2a
Otto <grentomato@gmail.com>
2025-01-11 12:52:34 -0600

    Complete Manager States

--- src/main/java/frc/robot/subsystems/Manager/ManagerStates.java
+++ src/main/java/frc/robot/subsystems/Manager/ManagerStates.java
@@ -1,27 +1,29 @@
 package frc.robot.Subsystems.Manager;
 
+import frc.robot.Subsystems.Algaer.AlgaerStates;
+import frc.robot.Subsystems.AutoAlign.AutoAlignStates;
 import frc.robot.Subsystems.Coraler.CoralerStates;
 import frc.robot.Subsystems.Elevator.ElevatorStates;
 import org.team7525.subsystem.SubsystemStates;
 
 public enum ManagerStates implements SubsystemStates {
-	IDLE("Idle", ElevatorStates.IDLE, CoralerStates.IDLE),
-	CLIMBING("Climbing", ElevatorStates.CLIMBING, CoralerStates.IDLE),
-	INTAKING_CORALER("Intaking through Coraler", ElevatorStates.IDLE, CoralerStates.INAKING),
-	INTAKING_ALGAE_LOW("Intaking Algae Low", ElevatorStates.ALGAE_LOW, CoralerStates.IDLE),
-	INTAKING_ALGAE_HIGH("Intaking Algae High", ElevatorStates.ALGAE_HIGH, CoralerStates.IDLE),
-	GOING_PROCESSOR("Going to Processor", ElevatorStates.ALGAE_PROCESSOR, CoralerStates.IDLE),
-	SCORING_PROCESSOR("Scoring at Processor", ElevatorStates.ALGAE_PROCESSOR, CoralerStates.IDLE),
-	AUTO_ALIGN_CLOSE("Aligning Close", ElevatorStates.IDLE, CoralerStates.IDLE),
-	AUTO_ALIGN_FAR("Aligning Close", ElevatorStates.IDLE, CoralerStates.IDLE),
-	SCORING_REEF("Scoring Reef", ElevatorStates.IDLE, CoralerStates.CORALING),
-	TRANSITIONING_SCORING_REEF("Transitioning Scoring", ElevatorStates.IDLE, CoralerStates.IDLE),;
+	IDLE("Idle", ElevatorStates.IDLE, CoralerStates.IDLE, AlgaerStates.IDLE, AutoAlignStates.OFF),
+	CLIMBING("Climbing", ElevatorStates.CLIMBING, CoralerStates.IDLE, AlgaerStates.IDLE, AutoAlignStates.OFF),
+	INTAKING_CORALER("Intaking at Coral Station", ElevatorStates.IDLE, CoralerStates.INAKING, AlgaerStates.IDLE, AutoAlignStates.OFF),
+	INTAKING_ALGAE_LOW("Intaking Algae Low", ElevatorStates.ALGAE_LOW, CoralerStates.IDLE, AlgaerStates.INTAKING, AutoAlignStates.OFF),
+	INTAKING_ALGAE_HIGH("Intaking Algae High", ElevatorStates.ALGAE_HIGH, CoralerStates.IDLE, AlgaerStates.INTAKING, AutoAlignStates.OFF),
+	GOING_PROCESSOR("Going to Processor", ElevatorStates.ALGAE_PROCESSOR, CoralerStates.IDLE, AlgaerStates.IDLE, AutoAlignStates.OFF),
+	SCORING_PROCESSOR("Scoring at Processor", ElevatorStates.ALGAE_PROCESSOR, CoralerStates.IDLE, AlgaerStates.SHOOTING, AutoAlignStates.OFF),
+	AUTO_ALIGN_CLOSE("Aligning Close", ElevatorStates.IDLE, CoralerStates.IDLE, AlgaerStates.IDLE, AutoAlignStates.DRIVING_REEF),
+	AUTO_ALIGN_FAR("Aligning Close", ElevatorStates.IDLE, CoralerStates.IDLE, AlgaerStates.IDLE, AutoAlignStates.DRIVING_REEF),
+	SCORING_REEF("Scoring Reef", ElevatorStates.IDLE, CoralerStates.CORALING, AlgaerStates.IDLE, AutoAlignStates.OFF),
+	TRANSITIONING_SCORING_REEF("Transitioning Scoring", ElevatorStates.IDLE, CoralerStates.IDLE, AlgaerStates.IDLE, AutoAlignStates.OFF),;
 
 
-	
+	// NOTE: Transitioning Scoring, AA, and any state meant to score on reef has a FILLER ELEVATOR AND POSSIBLY CORALER STATE
 
 
-	ManagerStates(String stateString, ElevatorStates elevatorState, CoralerStates coralerState) {
+	ManagerStates(String stateString, ElevatorStates elevatorState, CoralerStates coralerState, AlgaerStates algaerState, AutoAlignStates autoAlignState) {
 		this.stateString = stateString;
 		this.elevatorState = elevatorState;
 		this.coralerState = coralerState;
@@ -30,6 +32,8 @@ public enum ManagerStates implements SubsystemStates {
 	private String stateString;
 	private ElevatorStates elevatorState;
 	private CoralerStates coralerState;
+	private AlgaerStates algaerState;
+	private AutoAlignStates autoAlignState;
 
 	@Override
 	public String getStateString() {
@@ -42,5 +46,13 @@ public enum ManagerStates implements SubsystemStates {
 
 	protected CoralerStates getCoralerState() {
 		return coralerState;
+	}
+
+	protected AlgaerStates getAlgaerState() {
+		return algaerState;
+	}
+
+	protected AutoAlignStates getAutoAlignState() {
+		return autoAlignState;
 	}
 }
