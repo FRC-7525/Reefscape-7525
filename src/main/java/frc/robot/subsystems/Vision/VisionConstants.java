7371300845964ada06354af3d57d1dcd03043c38
Otto <105565584+GreenTomato5@users.noreply.github.com>
2025-02-09 21:58:02 -0600

    More camera configs (#77)
    * Fix Camera Configs
    
    * Apply Prettier format
    
    * UPDATE UPDATE UPDATE!
    
    * Update ascope config
    
    * Move 15 to the right thingy
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -17,7 +17,7 @@ public class VisionConstants {
 	public static final Rotation3d ROBOT_TO_BACK_CAMERA_ROTATION = new Rotation3d(0, Math.toRadians(15), Math.toRadians(180));
 	public static final Transform3d ROBOT_TO_BACK_CAMERA = new Transform3d(ROBOT_TO_BACK_CAMERA_TRALSLATION, ROBOT_TO_BACK_CAMERA_ROTATION);
 	public static final Translation3d ROBOT_TO_FRONT_CAMERA_TRANSLATION = new Translation3d(Units.inchesToMeters(11.697), Units.inchesToMeters(-11.81), Units.inchesToMeters(8.859));
-	public static final Rotation3d ROBOT_TO_FRONT_CAMERA_ROTATION = new Rotation3d(0, Math.toRadians(0), Math.toRadians(15));
+	public static final Rotation3d ROBOT_TO_FRONT_CAMERA_ROTATION = new Rotation3d(0, Math.toRadians(15), Math.toRadians(0));
 	public static final Transform3d ROBOT_TO_FRONT_CAMERA = new Transform3d(ROBOT_TO_FRONT_CAMERA_TRANSLATION, ROBOT_TO_FRONT_CAMERA_ROTATION);
 
 	public static final double CAMERA_DEBOUNCE_TIME = 0.5;
