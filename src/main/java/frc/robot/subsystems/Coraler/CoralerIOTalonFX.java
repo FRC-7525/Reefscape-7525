487fe38a3e666fe8b98391e40689bfc2d8141bae
Otto <grentomato@gmail.com>
2025-02-27 22:58:04 -0600

    switch fix duh

--- src/main/java/frc/robot/subsystems/Coraler/CoralerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/Coraler/CoralerIOTalonFX.java
@@ -5,22 +5,27 @@ import static frc.robot.GlobalConstants.*;
 import static frc.robot.Subsystems.Coraler.CoralerConstants.*;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.ReverseLimitValue;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.GlobalConstants.RobotMode;
+import frc.robot.Subsystems.Coraler.CoralerConstants.Real;
 
 public class CoralerIOTalonFX implements CoralerIO {
 
 	private final TalonFX velocityMotor;
 	private final PIDController velocityController;
-	private final DigitalInput beamBreak;
+	// private final DigitalInput beamBreak;
 	private double speedPoint;
 
 	public CoralerIOTalonFX() {
 		velocityMotor = new TalonFX(Real.WHEEL_MOTOR_CAN_ID);
 		velocityMotor.setNeutralMode(NeutralModeValue.Brake);
-		beamBreak = new DigitalInput(Real.DIO_PORT);
+		// beamBreak = new DigitalInput(Real.DIO_PORT);
 
 		velocityController = VELOCITY_CONTROLLER.get();
 		speedPoint = 0.0;
@@ -53,6 +58,7 @@ public class CoralerIOTalonFX implements CoralerIO {
 
 	@Override
 	public boolean hasGamepiece() {
-		return !beamBreak.get();
+		// return !beamBreak.get();
+		return velocityMotor.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround;
 	}
 }
