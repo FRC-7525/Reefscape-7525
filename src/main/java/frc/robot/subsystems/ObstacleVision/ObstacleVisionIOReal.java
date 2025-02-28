57ec93d81f833136fc789ec35b2a4842ae9e48e8
PotmanNob <Potmannob@outlook.com>
2025-02-28 12:49:48 -0600

    very questionable code

---
+++ src/main/java/frc/robot/subsystems/ObstacleVision/ObstacleVisionIOReal.java
@@ -0,0 +1,42 @@
+package frc.robot.Subsystems.ObstacleVision;
+
+import java.util.ArrayList;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.simulation.PhotonCameraSim;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import frc.robot.Subsystems.Drive.Drive;
+
+public class ObstacleVisionIOReal implements ObstacleVisionIO {
+
+    private PhotonCamera camera;
+    private Transform3d robotToCamera;
+    private ArrayList<Pose2d> obstacleList;
+
+    public ObstacleVisionIOReal(String cameraName, Transform3d robotToCamera) {
+
+        camera = new PhotonCamera(cameraName);
+        this.robotToCamera = robotToCamera;
+        obstacleList = new ArrayList<Pose2d>();
+    }
+
+    public ArrayList<Pose2d> getObstaclePoses() {
+        for (var result : camera.getAllUnreadResults()) {
+            for (var obstacle : result.getTargets()) {
+                Transform3d cameraToObject = obstacle.getBestCameraToTarget();
+                Pose2d robotPose = Drive.getInstance().getPose();
+
+                Translation3d transformRobotRelative = cameraToObject.getTranslation().rotateBy(robotToCamera.getRotation().unaryMinus());
+                Pose2d obstaclePose = robotPose.transformBy(new Transform2d(transformRobotRelative.toTranslation2d(), cameraToObject.getRotation().toRotation2d()));
+
+                obstacleList.add(obstaclePose);
+            }
+        }
+        return obstacleList;
+    }
+}
